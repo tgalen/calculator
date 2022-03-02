df1ca73409b98a244c9c33779cfaff7e8bf44f6f
tgalen <tgalen84@gmail.com>
2022-03-01 19:11:53 -0500

    add 'clear' and 'sqrt' button

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ screen = document.getElementById("screen");
 let selectNumber = (num) => {
     // pendingNumber += num;
     console.log(pendingNumber);
+
     pendingNumber.length < 9 ? (pendingNumber += num) : (pendingNumber = pendingNumber);
-    screen.innerHTML = pendingNumber;
+    screen.innerHTML = parseInt(pendingNumber);
+};
+
+let clearScreen = () => {
+    pendingNumber = "0";
+    screen.innerHTML = parseInt(pendingNumber);
+};
+
+let sqrRoot = () => {
+    num = parseInt(screen.innerHTML);
+    console.log(num);
+    screen.innerHTML = Math.sqrt(num).toFixed(9);
 };
