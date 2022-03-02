4a2d8c48e7a0fe168010927dc9e6d4ad8e296ebc
tgalen <tgalen84@gmail.com>
2022-03-02 11:29:32 -0500

    rename 'top-row' buttons

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ screen = document.getElementById("screen");
 let filterString = (numStr) => {
     // filter out leading 0
     let strToNum = parseInt(numStr);
-    console.log(strToNum);
     let numToStr = strToNum.toString();
-    console.log(numToStr);
     return numToStr;
 };
 
@@ -15,7 +13,6 @@ console.log(filterString("999"));
 
 let selectNumber = (num) => {
     pendingNumber.length > 0 ? (pendingNumber = filterString(pendingNumber)) : (pendingNumber = pendingNumber); // prevent an empty string being converted to a nunmber
-
     if (pendingNumber.includes(".") && num === ".") {
         pendingNumber += "";
     } else if (pendingNumber.length < 10) {
@@ -25,8 +22,6 @@ let selectNumber = (num) => {
     }
     pendingNumberToInt = parseInt(pendingNumber);
     screen.innerHTML = pendingNumberToInt;
-    console.log(pendingNumber);
-    console.log(pendingNumberToInt);
 };
 
 let clearScreen = () => {
